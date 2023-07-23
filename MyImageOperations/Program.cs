a390a80da19ed006ec0911f1e4319b042338e277
Zirael <igor.www.pl.com@gmail.com>
2023-07-23 22:06:45 +0200

    i dont know what ive done but it looks cool so yeah

--- MyImageOperations/Program.cs
+++ MyImageOperations/Program.cs
@@ -5,6 +5,9 @@ using System.Collections;
 using System.Drawing.Imaging;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Dynamic;
+using System.Security.Authentication;
+using System.IO.Ports;
 
 class Program
 {
@@ -19,11 +22,14 @@ class Program
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
 
-        for (int i = 0; i < bmp.Width; i++)
+        for (int x = 1; x < bmp.Width - 1; x++)
         {
-            for (int j = 0; j < bmp.Height; j++)
+            for (int y = 0; y < bmp.Height - 1; y++)
             {
-                Color pixelColor = bmp.GetPixel(i, j);
+                Color pixelColor = bmp.GetPixel(x, y);
+
+                int index = GetImgIndex(x, y, bmp.Width);
+
                 int red = pixelColor.R;
                 int green = pixelColor.G;
                 int blue = pixelColor.B;
@@ -33,32 +39,50 @@ class Program
                 float _g = (float)green;
                 float _b = (float)blue; 
 
-                decimal r = Math.Round((decimal)(2 * _r / 255)) * (255 / 2);
-                decimal g = Math.Round((decimal)(2 * _g / 255)) * (255 / 2);
-                decimal b = Math.Round((decimal)(2 * _b / 255)) * (255 / 2);
+                decimal r = Math.Round((decimal)(1 * _r / 255)) * (255 / 1);
+                decimal g = Math.Round((decimal)(1 * _g / 255)) * (255 / 1);
+                decimal b = Math.Round((decimal)(1 * _b / 255)) * (255 / 1);
 
-                Color ditherPixel = Color.FromArgb((int)r, (int)g, (int)b);
-                bmp.SetPixel(i, j, ditherPixel);
+                // JUST LOWERING DOWN THE DETAILS
+                //Color ditherPixel = Color.FromArgb((int)r, (int)g, (int)b);
+                //bmp.SetPixel(x, y, ditherPixel);
+
+                float errorR = red - (float)r;
+                float errorG = green - (float)g;
+                float errorB = blue - (float)b;
+
+                OffsetDither(x + 1, y    , bmp, errorR, errorG, errorB, 7);
+                OffsetDither(x - 1, y + 1, bmp, errorR, errorG, errorB, 3);
+                OffsetDither(x    , y + 1, bmp, errorR, errorG, errorB, 5);
+                OffsetDither(x + 1, y + 1, bmp, errorR, errorG, errorB, 1);
 
                 // INVERSE
                 //Color inversePixel = Color.FromArgb(255 - red, 255 - green, 255 - blue);
-                //bmp.SetPixel(i, j, inversePixel);
+                //bmp.SetPixel(x, y, inversePixel);
 
                 // BLACK OR WHITE
-                //if(gray < 128)
+
+                //Color afterDither = bmp.GetPixel(x, y);
+
+                //int _red = afterDither.R;
+                //int _green = afterDither.G;
+                //int _blue = afterDither.B;
+                //int _gray = (byte)(.299 * _red + .587 * _green + .114 * _blue);
+
+                //if (_gray < 128)
                 //{
-                //    bmp.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                //    bmp.SetPixel(x, y, Color.FromArgb(0, 0, 0));
                 //}
-                //else if(gray >= 128)
+                //else if (_gray >= 128)
                 //{
-                //    bmp.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                //    bmp.SetPixel(x, y, Color.FromArgb(255, 255, 255));
                 //}
 
                 // GRAYSCALE
                 //red = gray;
                 //green = gray;
                 //blue = gray;
-                //bmp.SetPixel(i, j, Color.FromArgb(red, green, blue));
+                //bmp.SetPixel(x, y, Color.FromArgb(red, green, blue));
             }
         }
 
@@ -72,6 +96,37 @@ class Program
 
         Console.WriteLine("To exit press any key");
         Console.ReadKey(true);
+    }
+
+    static void OffsetDither(int x, int y, Bitmap bmp, float errorR, float errorG, float errorB, int amount)
+    {
+        Color ditherPixel = bmp.GetPixel(x, y);
+        float newR = (float)ditherPixel.R + errorR * amount / 16;
+        float newG = (float)ditherPixel.G + errorG * amount / 16;
+        float newB = (float)ditherPixel.B + errorB * amount / 16;
+
+        if (newR >= 256)
+            newR = 255;
+        else if (newR < 0)
+            newR = 0;
+
+        if (newG >= 256)
+            newG = 255;
+        else if (newG < 0)
+            newG = 0;
+
+        if (newB >= 256)
+            newB = 255;
+        else if (newB < 0)
+            newB = 0;
+
+        Color newDitherColor = Color.FromArgb((int)newR, (int)newG, (int)newB);
+        bmp.SetPixel(x, y, newDitherColor);
+    }
+
+    static int GetImgIndex(int x, int y, int width)
+    {
+        return x + y * width;
     }
 }
 
