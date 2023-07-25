c21bff3c859be09f1756ec6857ff2b4d3442e260
Zirael <igor.www.pl.com@gmail.com>
2023-07-25 20:38:23 +0200

    changed stuff, it worked but i broke it

--- MyImageOperations/Program.cs
+++ MyImageOperations/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime;
 using System.Drawing;
 using System.Numerics;
 using System.Collections;
@@ -8,6 +9,7 @@ using System.Diagnostics;
 using System.Dynamic;
 using System.Security.Authentication;
 using System.IO.Ports;
+using System.Xml;
 
 class Program
 {
@@ -22,46 +24,54 @@ class Program
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
 
+        // original colors
+        int o_r = 0;
+        int o_g = 0;
+        int o_b = 0;
+
         for (int x = 1; x < bmp.Width - 1; x++)
         {
             for (int y = 0; y < bmp.Height - 1; y++)
             {
                 Color pixelColor = bmp.GetPixel(x, y);
 
-                int index = GetImgIndex(x, y, bmp.Width);
-
                 int red = pixelColor.R;
                 int green = pixelColor.G;
                 int blue = pixelColor.B;
                 int gray = (byte)(.299 * red + .587 * green + .114 * blue);
 
-                float _r = (float)red;
-                float _g = (float)green;
-                float _b = (float)blue; 
+                // Original colors
+                o_r = red;
+                o_g = green;
+                o_b = blue;
+            }
+        }
 
-                decimal r = Math.Round((decimal)(1 * _r / 255)) * (255 / 1);
-                decimal g = Math.Round((decimal)(1 * _g / 255)) * (255 / 1);
-                decimal b = Math.Round((decimal)(1 * _b / 255)) * (255 / 1);
+        Bitmap qBmp = Quantization(bmp);
+        Bitmap dBmp = FloydSteinberg(bmp, qBmp);
 
-                // JUST LOWERING DOWN THE DETAILS
-                //Color ditherPixel = Color.FromArgb((int)r, (int)g, (int)b);
-                //bmp.SetPixel(x, y, ditherPixel);
+        for (int x = 1; x < bmp.Width - 1; x++)
+        {
+            for (int y = 0; y < bmp.Height - 1; y++)
+            {
+                Color pixelColor = bmp.GetPixel(x, y);
 
-                float errorR = red - (float)r;
-                float errorG = green - (float)g;
-                float errorB = blue - (float)b;
-
-                OffsetDither(x + 1, y    , bmp, errorR, errorG, errorB, 7);
-                OffsetDither(x - 1, y + 1, bmp, errorR, errorG, errorB, 3);
-                OffsetDither(x    , y + 1, bmp, errorR, errorG, errorB, 5);
-                OffsetDither(x + 1, y + 1, bmp, errorR, errorG, errorB, 1);
+                int red = pixelColor.R;
+                int green = pixelColor.G;
+                int blue = pixelColor.B;
+                int gray = (byte)(.299 * red + .587 * green + .114 * blue);
+                
+                // GRAYSCALE
+                //red = gray;
+                //green = gray;
+                //blue = gray;
+                //bmp.SetPixel(x, y, Color.FromArgb(red, green, blue));
 
                 // INVERSE
                 //Color inversePixel = Color.FromArgb(255 - red, 255 - green, 255 - blue);
                 //bmp.SetPixel(x, y, inversePixel);
 
                 // BLACK OR WHITE
-
                 //Color afterDither = bmp.GetPixel(x, y);
 
                 //int _red = afterDither.R;
@@ -77,12 +87,6 @@ class Program
                 //{
                 //    bmp.SetPixel(x, y, Color.FromArgb(255, 255, 255));
                 //}
-
-                // GRAYSCALE
-                //red = gray;
-                //green = gray;
-                //blue = gray;
-                //bmp.SetPixel(x, y, Color.FromArgb(red, green, blue));
             }
         }
 
@@ -92,41 +96,96 @@ class Program
         Console.WriteLine("Please input new file path");
         string savePath = Console.ReadLine();
 
-        bmp.Save(savePath);
+        dBmp.Save(savePath);
 
         Console.WriteLine("To exit press any key");
         Console.ReadKey(true);
     }
 
-    static void OffsetDither(int x, int y, Bitmap bmp, float errorR, float errorG, float errorB, int amount)
+    static Bitmap FloydSteinberg(Bitmap original, Bitmap quant)
     {
-        Color ditherPixel = bmp.GetPixel(x, y);
-        float newR = (float)ditherPixel.R + errorR * amount / 16;
-        float newG = (float)ditherPixel.G + errorG * amount / 16;
-        float newB = (float)ditherPixel.B + errorB * amount / 16;
+        //Bitmap dBmp = null;
 
-        if (newR >= 256)
-            newR = 255;
-        else if (newR < 0)
-            newR = 0;
+        for (int x = 1; x < original.Width - 1; x++)
+        {
+            for (int y = 0; y < original.Height - 1; y++)
+            {
+                // Original colors
+                Color originalColor = original.GetPixel(x, y);
+                int o_red = originalColor.R;
+                int o_green = originalColor.G;
+                int o_blue = originalColor.B;
 
-        if (newG >= 256)
-            newG = 255;
-        else if (newG < 0)
-            newG = 0;
+                // Quant colors
+                Color quantColor = quant.GetPixel(x, y);
+                int q_red = quantColor.R;
+                int q_green = quantColor.G;
+                int q_blue = quantColor.B;
 
-        if (newB >= 256)
-            newB = 255;
-        else if (newB < 0)
-            newB = 0;
+                // Dither error
+                int errorR = o_red - q_red;
+                int errorG = o_green - q_green;
+                int errorB = o_blue - q_blue;
 
-        Color newDitherColor = Color.FromArgb((int)newR, (int)newG, (int)newB);
-        bmp.SetPixel(x, y, newDitherColor);
+                quant = OffsetDither(x + 1, y, quant, errorR, errorG, errorB, 7);
+                quant = OffsetDither(x - 1, y + 1, quant, errorR, errorG, errorB, 3);
+                quant = OffsetDither(x, y + 1, quant, errorR, errorG, errorB, 5);
+                quant = OffsetDither(x + 1, y + 1, quant, errorR, errorG, errorB, 1);
+            }
+        }
+
+        return quant;
     }
 
-    static int GetImgIndex(int x, int y, int width)
+    static Bitmap Quantization(Bitmap bmp)
     {
-        return x + y * width;
+        Bitmap quantizedBMP = null;
+
+        for (int x = 1; x < bmp.Width - 1; x++)
+        {
+            for (int y = 0; y < bmp.Height - 1; y++)
+            {
+                // Original colors
+                Color pixelColor = bmp.GetPixel(x, y);
+                int o_red = pixelColor.R;
+                int o_green = pixelColor.G;
+                int o_blue = pixelColor.B;
+
+                // Quantizated colors
+                int quantAmount = 1;
+                int q_r = (quantAmount * o_red / 255) * (255 / quantAmount);
+                int q_g = (quantAmount * o_green / 255) * (255 / quantAmount);
+                int q_b = (quantAmount * o_blue / 255) * (255 / quantAmount);
+
+                // QUANTIZATION
+                Color ditherPixel = Color.FromArgb(q_r, q_g, q_b);
+                bmp.SetPixel(x, y, ditherPixel);
+                quantizedBMP = bmp;
+            }
+        }
+
+        return quantizedBMP;
+    }
+
+    static Bitmap OffsetDither(int x, int y, Bitmap bmp, int errorR, int errorG, int errorB, int amount)
+    {
+        Color ditherPixel = bmp.GetPixel(x, y);
+        int newR = ditherPixel.R + errorR * amount / 16;
+        int newG = ditherPixel.G + errorG * amount / 16;
+        int newB = ditherPixel.B + errorB * amount / 16;
+
+        int clampedR =  Clamp(newR, 0, 255);
+        int clampedG = Clamp(newG, 0, 255);
+        int clampedB = Clamp(newB, 0, 255);
+
+        Color newDitherColor = Color.FromArgb(clampedR, clampedG, clampedB);
+        bmp.SetPixel(x, y, newDitherColor);
+        return bmp;
+    }
+
+    public static int Clamp(int value, int min, int max)
+    {
+        return (value < min) ? min : (value > max) ? max : value;
     }
 }
 
